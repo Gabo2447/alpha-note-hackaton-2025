1ef07f4ce790641a6f8f82fcb22ccd78d53f0e94
Gabo2447 <gabrieldeolival2@gmail.com>
2025-05-11 21:00:53 -0400

    fix(vercel): :rocket: arreglando bugs de deployment con vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [clerk()],
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   output: "server",
 
   vite: {
